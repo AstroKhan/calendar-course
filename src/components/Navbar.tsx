e2ef8d6f5973986d042e4c5fbdcbedc30d199b39
AstroGit <masaladosa6@gmail.com>
2022-02-01 11:57:27 +0300

    make login logic

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,11 +3,13 @@ import {Layout, Row, Menu} from 'antd';
 import {useHistory} from 'react-router-dom';
 import { RouteNames } from '../router';
 import { useTypedSelector } from '../hooks/useTypedSelector';
+import { AuthActionCreators } from '../store/reducers/auth/action-creators';
+import {useDispatch} from 'react-redux';
 
 const Navbar: FC = () => {
     const router = useHistory()
-    const {isAuth} = useTypedSelector(state => state.auth);
-    
+    const {isAuth, user} = useTypedSelector(state => state.auth);
+    const dispatch = useDispatch();
   return (
   <Layout.Header>
       <Row justify="end">
@@ -15,11 +17,11 @@ const Navbar: FC = () => {
             ? 
             <>
             <div style={{color: 'white'}}>
-                Astro
+                {user.username}
             </div>
             <Menu theme='dark' mode='horizontal' selectable={false}> 
             <Menu.Item 
-            onClick={() => console.log('Выйти')} 
+            onClick={() => dispatch(AuthActionCreators.logout())} 
             key={1}
             >
                 Выйти
---
+++ src/store/reducers/auth/action-creators.ts
@@ -0,0 +1,38 @@
+//import { AuthActionCreators } from './action-creators';
+import { AppDispatch } from "../../index";
+import { IUser } from "../../../models/IUser";
+import { AuthActionEnum, SetAuthAction, SetErrorAction, SetIsLoadingAction, SetUserAction } from "./types";
+import axios from 'axios';
+
+export const AuthActionCreators = {
+    setUser: (user: IUser): SetUserAction => ({type: AuthActionEnum.SET_USER, payload: user}),
+    setIsAuth: (auth: boolean): SetAuthAction => ({type: AuthActionEnum.SET_AUTH, payload: auth}),
+    SetIsLoading: (payload: boolean): SetIsLoadingAction => ({type: AuthActionEnum.SET_IS_LOADING, payload}),
+    SetError: (payload: string): SetErrorAction => ({type: AuthActionEnum.SET_ERROR, payload}),
+    login: (username: string, password: string) => async (dispatch: AppDispatch) => {
+        try {
+            dispatch(AuthActionCreators.SetIsLoading(true));
+            setTimeout( async () => {
+            const response = await axios.get<IUser[]>('./users.json');
+            const mockUser = response.data.find(user => user.username === username && user.password === password);
+            if (mockUser) {
+                localStorage.setItem('auth', 'true');
+                localStorage.setItem('username', mockUser.username);
+                dispatch(AuthActionCreators.setIsAuth(true));
+                dispatch(AuthActionCreators.setUser(mockUser));
+            } else {
+                dispatch(AuthActionCreators.SetError('Некорректный логин или пароль'));
+            }
+            dispatch(AuthActionCreators.SetIsLoading(false));
+            }, 1000)
+        } catch (e) {
+            dispatch(AuthActionCreators.SetError('Произошла ошибка при логине'));
+        }
+    },
+    logout: () => async (dispatch: AppDispatch) => {
+        localStorage.removeItem('auth')
+        localStorage.removeItem('username')
+        dispatch(AuthActionCreators.setUser({} as IUser));
+        dispatch(AuthActionCreators.setIsAuth(false));   
+    }
+}
