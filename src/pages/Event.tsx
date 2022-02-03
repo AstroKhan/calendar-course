9bcef57c187d62de5a66f125d5e18388852f7565
AstroGit <masaladosa6@gmail.com>
2022-02-03 13:22:24 +0300

    add event form

--- src/pages/Event.tsx
+++ src/pages/Event.tsx
@@ -1,10 +1,29 @@
-import React, { FC } from 'react';
+import React, { FC, useState } from 'react';
+import EventCalendar from '../components/EventCalendar';
+import {Layout, Row, Button, Modal} from 'antd';
+import EventForm from '../components/EventForm';
 
 const Event: FC = () => {
+    const [modalVisible, setModalVisible] = useState(false);
   return (
-  <div>
-      EVENT PAGE
-  </div>
+  <Layout>
+      <EventCalendar events={[]}/>
+      <Row justify='center'>
+          <Button 
+          onClick={() => setModalVisible(true)}
+          >
+            Добавить событие
+          </Button>
+      </Row>
+      <Modal 
+      title='Добавить событие'
+      visible={modalVisible}
+      footer={null}
+      onCancel={() => setModalVisible(false)}
+      >
+        <EventForm/>
+      </Modal>
+  </Layout>
   );
 };
 
